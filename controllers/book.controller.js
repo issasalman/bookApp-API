bd583a1d75f4a9da6a4c9408d9c16047927c2326
omar11taha <omartawaha95@gmail.com>
2021-09-13 18:37:24 +0300

    lab12

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,17 +1,25 @@
-'use strict';
-
-const bookModel = require('../models/book.model');
-
-
+"use strict";
+const bookModel = require("../models/book.model");
 const getBooks = (request, response) => {
-
-    bookModel.find((error, booksData) => {
-    response.json(booksData)
+  bookModel.find((error, booksData) => {
+    response.json(booksData);
   });
-
 };
-
-
+const createBook = (request, response) => {
+  const { title, description, status, email } = request.body;
+  const newBook = new bookModel({ title, description, status, email });
+  newBook.save();
+  response.json(newBook);
+};
+const deleteBook = (request, response) => {
+  console.log(request.params);
+  const bookId = request.params.book_id;
+  bookModel.deleteOne({ _id: bookId }, (error, deletedData) => {
+    response.json(deletedData);
+  });
+};
 module.exports = {
-    getBooks
-}
+  getBooks,
+  createBook,
+  deleteBook,
+};
