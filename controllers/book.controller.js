c317bc427e02a6c498a615f2c24d01e7f91c1526
issasalman <issasalmanhu1996@gmail.com>
2021-09-14 05:54:54 +0300

    ok

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -12,6 +12,35 @@ const getBooks = (request, response) => {
 };
 
 
+const createBook = (request, response) => {
+
+  
+  const { title, description, status ,email} = request.body;
+
+
+  const newBook = new bookModel({  title, description, status,email
+  });
+
+  newBook.save();
+
+  response.json(newBook);
+
+}
+
+
+
+const deleteBook = (request, response) => {
+  console.log(request.params);
+  const bookId = request.params.book_id;
+
+  bookModel.deleteOne({ _id:bookId }, (error, deletedData) => {
+
+    response.json(deletedData);
+  });
+
+}
+
+
 module.exports = {
-    getBooks
+    getBooks,createBook,deleteBook
 }
